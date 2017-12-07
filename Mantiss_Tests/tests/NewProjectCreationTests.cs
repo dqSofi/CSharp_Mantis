695b93257c6226385bac759359cc5000c4824091
_DQ_ <_DQ_@NOTEBOOK>
2017-12-08 00:18:54 +0300

    в тест воздания проекта добавлена проверка совпадения имени

--- Mantiss_Tests/tests/NewProjectCreationTests.cs
+++ Mantiss_Tests/tests/NewProjectCreationTests.cs
@@ -14,10 +14,17 @@ namespace Mantiss_Tests
         [Test]
         public void NewProjectCreationTest()
         {
-            ProjectData project = new ProjectData("project name");
-
+            ProjectData project = new ProjectData("newP");
             List<ProjectData> oldProjects = ProjectData.GetAllFromDB();
-
+            //проверка на существоание проекта с таким именем, в случае совпадения имен
+            //добавляется рандомные символы в конец названия
+            for (int i = 0; i < oldProjects.Count;i++)
+                {
+                if (project.Name == oldProjects[i].Name)
+                {
+                    project.Name = project.Name + GenerateRandomString(2);
+                }
+                }
             app.Project.CreateNewProject(project);
 
             List<ProjectData> newProjects = ProjectData.GetAllFromDB();
