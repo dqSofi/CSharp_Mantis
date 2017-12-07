af26973daae32f8edf0993767914d8f6e68c191b
_DQ_ <_DQ_@NOTEBOOK>
2017-12-07 22:54:45 +0300

    Подключен Linq2db, добавлено подключение к БД

---
+++ Mantiss_Tests/model/MantisDB.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LinqToDB;
+
+namespace Mantiss_Tests
+{
+    public class MantisDB : LinqToDB.Data.DataConnection
+    {
+        public MantisDB() : base("root") { }
+
+        public ITable<ProjectData> Projects { get { return GetTable<ProjectData>(); } }
+    }
+}
--- Mantiss_Tests/model/ProjectData.cs
+++ Mantiss_Tests/model/ProjectData.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LinqToDB.Mapping;
 
 namespace Mantiss_Tests
 {
+    [Table(Name = "mantis_project_table")]
     public class ProjectData
     {
+        [Column(Name = "id"), PrimaryKey, Identity]
+        public string ID { get; set; }
+
+        [Column(Name = "name")]
         public string Name { get; set; }
         
         public ProjectData(string name)
         {
             Name = name;
+        }
+        public static List<ProjectData> GetAllFromDB()
+        {
+            using (MantisDB db = new MantisDB())
+            {
+                return (from g in db.Projects select g).ToList();
+            }
         }
     }
 }
